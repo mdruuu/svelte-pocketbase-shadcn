787c07cd66488940f15b6e9a2e55bf03443932f3
mdruuu <andrew@panocp.com>
2024-07-08 20:07:36 -0600

    initial commit - pocketbase version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<h1 class="text-3xl font-bold mb-4">Welcome to SvelteKit</h1>
+<p class="mb-2">This blank project is made with the following: </p>
+<ul class="list-disc pl-5">
+    <li>SvelteKit-Typescript Svelte 4.0</li>
+    <li>Tailwind CSS</li>
+    <li>Shadcn-Svelte</li>
+    <li>Pocketbase</li>
+    <li>Unplugin-icons</li>
+    <li>Zod</li>
+</ul>
